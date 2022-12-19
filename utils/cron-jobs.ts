7f729b5b80c8e773adfb40013f3ea6c05cf173b2
Nikolay Chankov <nikolay.chankov@gostartups.net>
2022-12-19 09:26:02 +0200

    cron-job time update change

--- utils/cron-jobs.ts
+++ utils/cron-jobs.ts
@@ -4,33 +4,33 @@ import { updateCurrencyMarketData } from "../services/coins.market.service";
 import Logger from "./logger";
 
 export const updateCoinsData = () => {
-    //At every 30 seconds.
+    //At every 43 seconds.
     new CronJob('*/43 * * * * *', async () =>{
        await updateCurrencyMarketData()
-           .catch(err => Logger.error(`Failed to udpate currency market data: ${err}`));
+           .catch(err => Logger.error(`Failed to update minute currency market data: ${err}`));
     }).start();
 
-    //At every 40th minute.
-    new CronJob('*/40 * * * *', async () =>{
+    //At every 43th minute.
+    new CronJob('*/43 * * * *', async () =>{
         await updateChartHistory(1)
-            .catch(err =>  Logger.error(`Failed to udpate currency market data: ${err}`));
+            .catch(err =>  Logger.error(`Failed to update day currency market data: ${err}`));
     }).start();
 
-    //At minute 10 past every 2nd hour.
-    new CronJob('10 */2 * * * ', async () => {
+    //At minute 11 past every 2nd hour.
+    new CronJob('11 */2 * * * ', async () => {
         await updateChartHistory(7)
-            .catch(err =>  Logger.error(`Error when updating week requests: '${err.message}`));
+            .catch(err =>  Logger.error(`Failed to update week market data: '${err.message}`));
     }).start();
 
-    //At minute 30 past every 12th hour
-    new CronJob('30 */12 * * *', async () => {
+    //At minute 31 past every 12th hour
+    new CronJob('31 */12 * * *', async () => {
         await updateChartHistory(30)
-            .catch(err =>  Logger.error(`Error when updating month requests: '${err.message}`));
+            .catch(err =>  Logger.error(`Failed to update month market data: '${err.message}`));
     }).start();
 
-    //At minute 15 past every 24th hour.
-    new CronJob('10 */24 * * *', async () => {
+    //At minute 3 past every 24th hour.
+    new CronJob('3 */24 * * *', async () => {
         await updateChartHistory(365)
-            .catch(err =>  Logger.error(`Error when updating year requests: '${err.message}`));
+            .catch(err =>  Logger.error(`Failed to update year market data: '${err.message}`));
     }).start();
 }
